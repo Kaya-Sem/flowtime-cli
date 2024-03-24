9bdc3494ad043d6b8c01800acfe5730c2100bd64
Kaya-Sem <kayasemvc@gmail.com>
2024-03-24 21:50:57 +0100

    added basic functionality

--- flowtime.py
+++ flowtime.py
@@ -1,27 +1,54 @@
-import argparse
+import click
+import csv
+import os
+
+# TODO
+# create config in .config
+# save in csv so they can graph it
+
+
+@click.command()
+@click.argument(
+    "action",
+    type=click.Choice(["start", "stop"]),
+    metavar="ACTION",
+    required=True,
+)
+def flowmodoro(action):
+    """A minimal distraction, maximum utility flowmodoro timer
+
+    actions: start, stop"""
+    click.echo(f"given action: {action}!")
+    create_history_file()
+
+
+def start_timer():
+    pass
+
+
+def create_history_file(filename="flowmodoro_history.csv", path=""):
+    # Get the home directory
+    home_dir = os.path.expanduser("~")
+
+    # Construct the full path to the file
+    file_path = os.path.join(home_dir, path, filename)
+
+    # Define the header
+    fieldnames = [
+        "date",
+        "start_time",
+        "end_time",
+        "focused_time",
+        "break_time",
+        "#focusblocks",
+        "#breaks",
+    ]
+
+    # Write an empty CSV file with header
+    with open(file_path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Manage flowtime timer")
-    actions = parser.add_mutually_exclusive_group(required=True)
-
-    actions.add_argument("start", help="start a new flowtime timer")
-    actions.add_argument("stop", help="stop the flowtime timer")
-    actions.add_argument("break", help="take a break")
-
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="log info to the terminal",
-        action="store_true",
-    )
-    args = parser.parse_args()
-
-    if args.verbose:
-        print(args)
-        print("verbosity turned on")
-
-    if args.start:
-        print(" start called")
-    elif args.stop:
-        print("stop called")
+    flowmodoro()
